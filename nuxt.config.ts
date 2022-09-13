9fc45390bde5360e33aa7028e70064bd2b7d0283
jondmoon <jon.moon@covenant.edu>
2022-09-13 14:44:52 -0400

    1 file modified, 2 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  modules: ['@pinia/nuxt'],
+});
---
+++ plugins/vuetify.js
@@ -0,0 +1,21 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, fa } from 'vuetify/iconsets/fa-svg';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'fa',
+      aliases,
+      sets: {
+        fa,
+      },
+    },
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
