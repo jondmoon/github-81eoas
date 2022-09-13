f14e8a05761505599685a228309132177f8a9cf4
jondmoon <jon.moon@covenant.edu>
2022-09-13 14:52:14 -0400

    3 files added

---
+++ pages/plays/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="mt-2" cols="12">
+        <strong>Recent Plays</strong>
+      </v-col>
+
+      <v-col v-for="j in 6" :key="j" cols="6" md="2">
+        <v-sheet height="150"></v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
